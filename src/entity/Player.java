919efb6bf7df799f56ea3c11ef9974d21cf5ccd0
valentinkaaaa <st8633533@stud.duikt.edu.ua>
2024-12-08 13:03:03 +0200

    Add files via upload

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -13,18 +13,33 @@ public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
 	private BufferedImage up2;
+	
+	public final int screenX;
+	public final int screenY;
+	public int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
+        
+        screenX = gp.screenWidth/2 - gp.tileSize/2;
+        screenY = gp.screenHeight/2 - gp.tileSize/2;
+        
+        solidArea = new Rectangle();
+        solidArea.x = 8;
+        solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+        solidArea.width = 32;
+        solidArea.height = 32;
 
         setDefaultValues();
         getPlayerImage();
     }
 
     public void setDefaultValues() {
-        x = 100;
-        y = 100;
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
         speed = 4;
         direction = "down";
     }
@@ -56,20 +71,36 @@ public class Player extends Entity {
     		
             if (keyH.upPressed == true) {
             	direction = "up";
-                y -= speed;
             } 
             else if (keyH.downPressed == true) {
             	direction = "down";
-                y += speed;
             } 
             else if (keyH.leftPressed == true) {
             	direction = "left";
-                x -= speed;
             } 
             else if (keyH.rightPressed == true) {
             	direction = "right";
-                x += speed;
             }
+            //CHECK TILE COLLISION
+            collisionOn = false;
+            gp.cChecker.checkTile(this);
+            
+        
+		    int objIndex = gp.cChecker.checkObject(this, true);
+		    pickUpObject(objIndex);
+            
+            //IF COLLISION IS FALSE, PLAYER CANT MOVE
+            if(collisionOn == false) {
+            	
+            	switch(direction) {
+            	case "up": worldY -= speed; break;
+            	case "down": worldY += speed; break;
+            	case "left": worldX -= speed; break;
+            	case "right": worldX += speed; break;
+            		
+            	}
+            }
+            
             
             spriteCounter++;
             if(spriteCounter > 12) {
@@ -83,6 +114,54 @@ public class Player extends Entity {
             }
     		
     	}
+    }
+    
+    public void pickUpObject(int i) {
+    	
+    	if(i != 999) {
+    		
+    		String objectName = gp.obj[i].name;
+    		
+    		
+    		switch(objectName) {
+    		case "Key":
+    			gp.playSE(2);
+    			hasKey++;
+    			gp.obj[i] = null;
+    			gp.ui.showMessage("You got a key!");
+    		 break;
+    		case "Door":
+    			if(hasKey > 0) {
+        			gp.playSE(4);
+                    gp.obj[i] = null;
+    				hasKey--;
+    				gp.ui.showMessage("You openned the door!");
+    				
+    			}
+    			else {
+    				gp.ui.showMessage("You need a key!");
+
+    				
+    			}
+        		break;
+    		case "Boots":
+    			gp.playSE(3);
+                speed += 2;
+    			gp.obj[i] = null;
+				gp.ui.showMessage("Speed up!");
+                break;
+    		case "Chest":
+    			gp.ui.gameFinished = true;
+    			gp.stopMusic();
+    			gp.playSE(4);
+    			break;
+    			
+    		
+    		}
+    		
+    		
+    	}
+    	
     }
 
     public void draw(Graphics2D g2) {
@@ -124,7 +203,7 @@ public class Player extends Entity {
 		  }
     	  break;
       }
-      g2.drawImage(image, x, y, gp.titleSize, gp.titleSize, null);
+      g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     	  
     
     
--- src/game2D/GamePanel.java
+++ src/game2D/GamePanel.java
@@ -1,6 +1,8 @@
 package game2D;
 
 import entity.Player;
+import object.SuperObject;
+import tile.TileManager;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,34 +10,56 @@ import java.awt.*;
 public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
-    final int originalTitleSize = 16; //16x16 tile
+    final int originalTileSize = 16; //16x16 tile
     final int scale = 3;
 
-    public final int titleSize = originalTitleSize * scale; //48X48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = titleSize * maxScreenCol; //768 pixels
-    final int screenHeight = titleSize * maxScreenRow; //576 pixels
+    public final int tileSize = originalTileSize * scale; //48X48 tile
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    public final int screenHeight = tileSize * maxScreenRow; //576 pixels
+    
+    //WORLD SETTINGS
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+   
 
     //FPS
     int FPS = 60;
-
+    
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
+    Sound music = new Sound();
+    Sound se = new Sound();
+    public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
+    public UI ui = new UI(this);
     Thread gameThread;
-    Player player = new Player(this,keyH);
-
-    //Set players default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
-
-    public GamePanel() {
+    
+    //ENTITY AND OBJECT
+    
+    public Player player = new Player(this,keyH);
+    public SuperObject obj[] = new SuperObject[10];
+    
+    
+	public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
     }
+	
+	public void setUpGame() {
+		
+		
+		aSetter.setObject();
+		playMusic(0);
+		
+		
+		
+		
+	}
 
     public void startGameThread() {
 
@@ -82,8 +106,47 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
+        
+        //TILE
+        tileM.draw(g2);
+        
+        //OBJECT
+        for(int i = 0; i < obj.length; i++) {
+        	if(obj[i] != null) {
+        		obj[i].draw(g2, this);
+        	}
+        	
+        }
+        
+        
+        //PLAYER
         player.draw(g2);
+        
+        //UI
+        ui.draw(g2);
+        
         g2.dispose();
 
     }
+    
+    public void playMusic(int i) {
+    	
+    	music.setFile(i);
+    	music.play();
+    	music.loop();
+    	
+    }
+    public void stopMusic() {
+    	
+    	music.stop();
+    	
+    }
+    
+    public void playSE(int i) {
+    	
+    	se.setFile(i);
+    	se.play();
+    	
+    }
+    
 }
